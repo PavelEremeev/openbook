a0a790bce04aace5cadc70aa50b050be2811726f
PavelEremeev <pe19950520@yandex.ru>
2021-07-14 22:40:39 +0300

    add grid to layout

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -5,7 +5,7 @@ export default function NewBook({ title, author }) {
     return (
         <section className='new-book'>
             <div className='new-book__container'>
-                <img className='new-book__cover'></img>
+                <div className='new-book__cover'></div>
                 <h4 className='new-book__title'>{title}</h4>
                 <h6 className='new-book__author'>{author}</h6>
             </div>
