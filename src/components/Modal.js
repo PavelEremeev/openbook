41cd04ae35dd8720ac4b58f8528ae1065e571874
PavelEremeev <pe19950520@yandex.ru>
2022-01-15 19:35:57 +0300

    style: добавил стили к Модальному окну

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './Modal.css';
 
 
-export default function Modal() {
+export default function Modal({ active, setActive, children }) {
 
+	function handlePopupIsOpen() {
+		setActive(false)
+	}
 
 	return (
-		<div className='modal'>
-			<div className='modal__container'>
-
+		<div className={active ? 'modal modal_active' : 'modal'} onClick={handlePopupIsOpen}>
+			<div className={active ? 'modal__container modal__container_active' : 'modal__container'} onClick={e => e.stopPropagation()}>
+				{children}
 			</div>
 		</div>
 	)
--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 import './NewBook.css'
 
-// адовый хардкод в рамках тестирования.
+
+
+// хардкод в рамках тестирования.
 
 const link = 'http://covers.openlibrary.org/b/isbn/'
 
 
 export default function NewBook({ title, author, cover, data }) {
-    return (
-        <section className='new-book'>
-            <div className='new-book__container'>
-                <img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
-                <h4 className='new-book__title'>{title}</h4>
-                <h6 className='new-book__author'>{author}</h6>
-            </div>
-        </section>
-    )
+
+	const [active, setActive] = useState(false);
+
+	return (
+		<section className='new-book'>
+			<div className='new-book__container'>
+				<img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
+				<h4 className='new-book__title'>{title}</h4>
+				<h6 className='new-book__author'>{author}</h6>
+			</div>
+			<button onClick={() => setActive(true)}>Поподробнее</button>
+			<Modal active={active} setActive={setActive}>
+				<div className='new-book__container'>
+					<img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
+					<h4 className='new-book__title'>{title}</h4>
+					<h6 className='new-book__author'>{author}</h6>
+				</div>
+			</Modal>
+		</section>
+
+	)
 }
