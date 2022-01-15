624dee042dee081f2c42dc96ef86d0319bd384b6
PavelEremeev <pe19950520@yandex.ru>
2022-01-15 21:15:32 +0300

    style: изменение стилей во всем приложений.

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -7,13 +7,13 @@ const link = 'http://covers.openlibrary.org/b/isbn/'
 
 
 export default function NewBook({ title, author, cover, data }) {
-    return (
-        <section className='new-book'>
-            <div className='new-book__container'>
-                <img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
-                <h4 className='new-book__title'>{title}</h4>
-                <h6 className='new-book__author'>{author}</h6>
-            </div>
-        </section>
-    )
+	return (
+		<section className='new-book'>
+			<div className='new-book__container'>
+				<img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
+				<h4 className='new-book__title'>{title}</h4>
+				<h6 className='new-book__author'>{author}</h6>
+			</div>
+		</section>
+	)
 }
