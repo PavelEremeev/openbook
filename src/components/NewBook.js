476771751e8605da515d5d8e5a427e41dc4e4322
PavelEremeev <pe19950520@yandex.ru>
2022-02-04 21:19:49 +0300

    fix: Вынес ссылку в отдельную константу

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './NewBook.css'
 
+import { LINK } from '../utils/constants'
 
 
-// хардкод в рамках тестирования.
 
-const link = 'http://covers.openlibrary.org/b/isbn/'
 
 
 export default function NewBook({ title, author, cover, onClick }) {
@@ -14,7 +13,7 @@ export default function NewBook({ title, author, cover, onClick }) {
 	return (
 		<section className='new-book' onClick={onClick}>
 			<div className='new-book__container'>
-				<img className='new-book__cover' src={`${link}${cover}-M.jpg`} alt={title}></img>
+				<img className='new-book__cover' src={`${LINK}${cover}-M.jpg`} alt={title}></img>
 				<h4 className='new-book__title'>{title}</h4>
 				<h6 className='new-book__author'>{author}</h6>
 			</div>
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+import react from "react";
+
+export const LINK = 'http://covers.openlibrary.org/b/isbn/';
